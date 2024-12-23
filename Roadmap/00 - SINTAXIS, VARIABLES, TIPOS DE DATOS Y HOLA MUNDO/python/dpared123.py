8f9f5ca4124e0ad66746516de5c38f8db78ff638
dpared123 <agustin2019pared@gmail.com>
2024-12-22 21:01:47 -0300

    dpared123.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dpared123.py
@@ -0,0 +1,24 @@
+#https://www.python.org/
+
+""" 
+varias 
+lineas
+de
+comentario
+"""
+´´´
+husadoiahsdkj
+´´´
+#Crea una variable (y una constante si el lenguaje lo soporta)
+objete = mesa 
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+nombre= "Juan" #string 
+edad=20 # int
+altura= 1.80 # float
+my_bolean= true
+my_bolean= false
+my_string= "hola"
+
+print("hola")
+
